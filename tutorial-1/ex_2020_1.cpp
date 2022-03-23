6f8aeaf1f80b36b734da354ec2c18ca4576556fb
GianMarcoCoppari <gianmarco.coppari@gmail.com>
2022-03-23 16:26:24 +0100

    Creation of CrystalBall PDF and parameters.

--- tutorial-1/ex_2020_1.cpp
+++ tutorial-1/ex_2020_1.cpp
@@ -1,3 +1,30 @@
-void crystalball() {
+// Coppari Gian Marco
+// University of Bologna - LM in Physics, Nuclear & Subnuclear Physics Student
 
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooCBShape.h"
+#include "TCanvas.h"
+#include "RooPlot.h"
+#include "TAxis.h"
+
+using namespace RooFit;
+
+
+void crystalball(double xmin, double xmax, int entries) {
+	// Set the domain
+	// double const xmin{ -10 };
+	// double const xmax{ 10 };
+
+	// Parameters of the CrystalBall PDF
+	RooRealVar mean{ "mean", "mean", 0, xmin, xmax};	// mean value
+	RooRealVar sigma{ "sigma", "sigma", 1, xmin, xmax };	// std. dev. of the gaussian part
+	RooRealVar alpha{ "alpha", "alpha", 1.5, xmin, xmax };
+	RooRealVar n{ "n", "n", 1.5, xmin, xmax };
+
+	// Create CrystalBall PDF, of a random variable x
+	RooRealVar x{"x", "x", xmin, xmax};
+	RooCBShape cb{"cb", "cb", x, mean, sigma, alpha, n};
+
+	
 }
