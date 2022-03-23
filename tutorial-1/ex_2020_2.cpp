f794f2ccc0b0c46eec5d463249fd76a07c52af13
GianMarcoCoppari <gianmarco.coppari@gmail.com>
2022-03-23 18:00:27 +0100

    Generate simulated data and plot on frames.

--- tutorial-1/ex_2020_2.cpp
+++ tutorial-1/ex_2020_2.cpp
@@ -20,5 +20,19 @@ void exponential(double xmin, double xmax, int entries, int nbins) {
 	RooRealVar x{ "x", "x", xmin, xmax };
 	RooExponential expo{ "expo", "expo", x, lambda };
 
+	// Create a frame for x random variable a plot
+	RooPlot* frame = x.frame(Title("Exponential PDF"));
+	expo.plotOn(frame, LineColor(kRed));
+
+	// Generate a binned dataset of 1000 events in x from gauss
+	x.setBins(nbins);
+	RooDataHist* data = expo.generateBinned(x, entries);
+
+	// Make a second plot frame in x and draw both the
+	// data and the pdf in the frame
+	auto frame2 = x.frame(Title("Exponential PDF with data"));
+	data->plotOn(frame2);
+	expo.plotOn(frame2);
+
 	
 }
