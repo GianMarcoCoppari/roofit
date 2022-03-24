f8d32f2f941c4d84228bff281436e682ac3ee724
GianMarcoCoppari <gianmarco.coppari@gmail.com>
2022-03-24 12:10:55 +0100

    Import .root file and creation of a binned dataset.

--- tutorial-1/ex_12_2.cpp
+++ tutorial-1/ex_12_2.cpp
@@ -1,3 +1,24 @@
-void b0s_invariant() {
+// Coppari Gian Marco
+// University of Bologna - LM in Physics, Nuclear & Subnuclear Physics Student
 
+#include "RooRealVar.h"
+#include "RooDataHist.h"
+#include "RooBreitWigner.h"
+
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TFile.h"
+
+using namespace RooFit;
+
+
+void b0s_invariant(double xmin, double xmax) {
+	// Define our observable
+	RooRealVar x{ "x", "x", xmin, xmax };
+
+	// Create a histogram and import it inside a RooDataHist object
+	auto file = TFile::Open("B0sInvariantMass.root");
+	auto h = static_cast<TH1F*>(file->Get("massaB0")); // h is pointer to a TH1F object.
+
+	RooDataHist dh{ "dh", "dh", x, RooFit::Import(*h) }; 
 }
