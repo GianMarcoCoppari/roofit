d8db8767dc5e7bf1da5df55dd9016352b263f11f
GianMarcoCoppari <gianmarco.coppari@gmail.com>
2022-03-24 12:45:42 +0100

    Updated structure. Model for the data analysis is created inside make_model function.

--- tutorial-1/ex_12_2.cpp
+++ tutorial-1/ex_12_2.cpp
@@ -1,8 +1,10 @@
 // Coppari Gian Marco
 // University of Bologna - LM in Physics, Nuclear & Subnuclear Physics Student
 
+#include "RooWorkspace.h"
 #include "RooRealVar.h"
 #include "RooDataHist.h"
+#include "RooGaussian.h"
 #include "RooBreitWigner.h"
 
 #include "TCanvas.h"
@@ -11,14 +13,40 @@
 
 using namespace RooFit;
 
+void make_model(RooWorkspace&);
+void use_model(RooWorkspace&);
+
 
 void b0s_invariant(double xmin, double xmax) {
-	// Define our observable
-	RooRealVar x{ "x", "x", xmin, xmax };
+	
+
+	RooWorkspace WS{ "WS" };
+	make_model(WS);
+	use_model(WS);
+
+}
+
+void make_model(RooWorkspace& W) {
+	// Define observable
+	RooRealVar x{ "x", "x", 0, 10};
+
+	// Define parameters: mean for both distributions, sigma for gaussian data, gamma for breit-wigner
+	RooRealVar mean{ "mean", "mean", 5.5, 5, 6, "GeV" };
+	RooRealVar sigma{ "sigma", "sigma", 0.2, 0, 1, "GeV" };
+	RooRealVar width{ "gamma", "gamma", 0, 1, "GeV" };
+
+	// Define models: breit-wigner and gaussian models
+	RooGauusian gaus{ "gauss", "gaus", x, mean, sigma };
+	RooBreitWigner breit{ "breitwigner", "breitwigner", x, mean, width };
 
 	// Create a histogram and import it inside a RooDataHist object
 	auto file = TFile::Open("B0sInvariantMass.root");
 	auto h = static_cast<TH1F*>(file->Get("massaB0")); // h is pointer to a TH1F object.
 
-	RooDataHist dh{ "dh", "dh", x, RooFit::Import(*h) }; 
+	RooDataHist dh{ "dh", "dh", x, RooFit::Import(*h) };
+
+	W.import(gaus);
+	W.import(breit);
+	W.import(dh);
 }
+// void use_model(RooWorkspace&);
